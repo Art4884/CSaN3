9ec58c84e6109a3298b82b974007bc9e62e7c7ef
Art4884 <artyandui@gmail.com>
2021-06-02 13:53:56 +0300

    first commit

---
+++ ksis-3/Server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace ksis_3
+{
+    public class ServerObject
+    {
+        static TcpListener tcpListener; 
+        List<ClientObject> clients = new List<ClientObject>();
+        List<string> history = new List<string>();
+
+        protected internal void AddConnection(ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+        }
+        protected internal void RemoveConnection(string id)
+        {
+            // получаем по id закрытое подключение
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            if (client != null)
+                clients.Remove(client);
+        }
+
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(tcpClient, this, history);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process)); //новый поток для обслуживания нового клиента
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            history.Add(message + "\n");
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Id != id) 
+                {
+                    clients[i].Stream.Write(data, 0, data.Length); 
+                }
+            }
+        }
+
+        protected internal void PrintHistory(NetworkStream Stream)
+        {
+            foreach (String message in history)
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                Stream.Write(data, 0, data.Length);
+            }
+        }
+
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop();
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close();
+            }
+            Environment.Exit(0);
+        }
+    }
+}
